aae9ada99cf6621a56d148ef3e4cbe4546df4705
Derrick Mei <derrick.mei.168@gmail.com>
2019-11-20 19:09:09 -0600

    changed Navbar.css

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './navbar.css';
+import './Navbar.css';
 
 import Dashboard from './Dashboard';
 import OpenIssues from './Open';
