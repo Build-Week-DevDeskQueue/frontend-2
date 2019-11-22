39cc70324573c2e29375614ff5242e0f0957b853
nottolivc <nottolivc@gmail.com>
2019-11-22 00:25:57 +0000

    'updated'

--- src/components/Open.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Open() {
-  return <div>Open Tickets</div>;
-}
--- src/components/Resolved.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Resolved() {
-  return <div>Resolved tickets</div>;
-}
